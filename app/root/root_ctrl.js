117e93ccd437575c652d79802bd4e8d33bc1b5b9
Daniel <daniel@ubermedia.com>
2017-01-20 11:44:08 -0800

    add propublica data

--- app/root/root_ctrl.js
+++ app/root/root_ctrl.js
@@ -1,24 +1,36 @@
 var app = angular.module('app');
 
-app.controller('rootController', function($scope, $rootScope, $window, rootServices, commServices) {
+app.controller('rootController', function($scope, $rootScope, $window, rootServices, houseCommittees, senateCommittees) {
 
-    $scope.committees = [];
-    _.forEach(commServices, function(committee) {
-        $scope.committees.push(committee)
-    })
+    $scope.houseCommittees = houseCommittees.committees;
+
+    $scope.senateCommittees = senateCommittees.committees;
+
     if ($window.localStorage["countoncongress-userSignedIn"]) {
         $scope.userData = $rootScope.twitterData;
     }
-    console.log("+++ 12 root_ctrl.js $scope.userData: ", $scope.userData)
 
-    var getTwitterData = function () {
-        console.log("+++ 15 root_ctrl.js Getting Twitter Data")
+    $scope.selectHouse = function() {
+        $scope.houseSelected = true;
+        $scope.senateSelected = false;
+        $scope.houseSelectedClass = "btn-primary";
+        $scope.senateSelectedClass = "btn-default";
+        $scope.committeeDataSelected = "houseCommittees"
+    }
+
+    $scope.selectSenate = function() {
+        $scope.houseSelected = false;
+        $scope.senateSelected = true;
+        $scope.houseSelectedClass = "btn-default";
+        $scope.senateSelectedClass = "btn-primary";
+    }
+
+    $scope.selectHouse()
+
+    var getTwitterData = function() {
         rootServices.userData()
             .then(function(twitterData) {
-                console.log("+++ 18 root_ctrl.js twitterData: ", twitterData)
                 $rootScope.twitterData = twitterData.data.twitterData;
-                console.log("+++ 20 root_ctrl.js $rootScope.twitterData: ", $rootScope.twitterData)
-                console.log("+++ 21 root_ctrl.js $scope.userData: ", $scope.userData)
             })
     }
 
@@ -33,11 +45,17 @@ app.controller('rootController', function($scope, $rootScope, $window, rootServi
             })
     }
 
-    $scope.sendTweet = function(message, name, committeeIndex, memberIndex) {
+    $scope.sendTweet = function(message, twitterHandle, position, chamber, index) {
         rootServices.sendTweet(message)
             .then(function(result) {
-                $scope.committees[committeeIndex][memberIndex].chairman.message = $scope.committees[committeeIndex][memberIndex].chairman.twitterHandle + " ";
-                $scope.alert.addAlert('Tweet Sent to ' + name, 'success');
+                if (chamber === "house") {
+                    $scope.houseCommittees[index][position].message = '@' + $scope.houseCommittees[index][position].twitter_account + " "
+                }
+                if (chamber === "senate") {
+                    $scope.senateCommittees[index][position].message = '@' + $scope.senateCommittees[index][position].twitter_account + " "
+                }
+
+                $scope.alert.addAlert('Tweet Sent to @' + twitterHandle, 'success');
             })
             .catch(function(result) {
                 var response = JSON.parse(result.data.data)
@@ -63,3 +81,62 @@ app.controller('rootController', function($scope, $rootScope, $window, rootServi
 
     getTwitterData();
 });
+
+
+
+// $scope.senate = senateServices;
+// $scope.house = houseServices;
+
+
+// $scope.matchedHouse = [];
+// $scope.matchedSenate = [];
+
+// var matchHouse = function() {
+//     _.forEach($scope.house.committees[0].committees, function(committee) {
+
+//         var matchedData = {
+//             committee: committee,
+//             chair: null,
+//             ranking_member: null
+
+//         };
+
+//         _.forEach($scope.house.members[0].members, function(member) {
+//             if (member.id === committee.chair_id) {
+//                 matchedData.chair = member;
+//             }
+//             if (member.id === committee.ranking_member_id) {
+//                 matchedData.ranking_member = member;
+//             }
+
+//         })
+//         $scope.matchedHouse.push(matchedData)
+//     })
+//     console.log("+++ 30 root_ctrl.js $scope.matchedHouse: ", JSON.stringify($scope.matchedHouse, null, "\t"));
+// }
+
+
+
+// var matchSenate = function() {
+//     _.forEach($scope.senate.committees[0].committees, function(committee) {
+
+//         var matchedData = {
+//             committee: committee,
+//             chair: null
+//         };
+
+//         _.forEach($scope.senate.members[0].members, function(member) {
+//             if (member.id === committee.chair_id) {
+//                 matchedData.chair = member;
+//             }
+//             if (member.id === committee.ranking_member_id) {
+//                 matchedData.ranking_member = member;
+//             }
+//         })
+//         $scope.matchedSenate.push(matchedData)
+//     })
+//     console.log("+++ 30 root_ctrl.js $scope.matchedSenate: ", JSON.stringify($scope.matchedSenate, null, "\t"));
+// }
+
+// // matchHouse();
+// matchSenate();
