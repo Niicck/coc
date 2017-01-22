8762541b19a02fe789ddc8aa56a0c0995274e0c5
Daniel <daniel@ubermedia.com>
2017-01-22 14:42:29 -0800

    Add avgs calculations

--- app/root/root_ctrl.js
+++ app/root/root_ctrl.js
@@ -83,8 +83,8 @@ app.controller('rootController', function($scope, $rootScope, $window, rootServi
     getTwitterData();
 // THIS CODE BELOW PULLS CONGRESS' COMMISSIONS AND MEMBERS DATA FROM PROPUBLICA AND MERGES IT PER HOUSE. TO RUN IT: UN COMMENT matchHouse() OR matchSenate() AND LOAD THE PAGE. THE MERGED DATA WILL SHOW UP ON THE CONSOLE.
 
-// $scope.senate = senateServices;
-// $scope.house = houseServices;
+$scope.senate = senateServices;
+$scope.house = houseServices;
 
 
 // $scope.matchedHouse = [];
@@ -143,5 +143,62 @@ app.controller('rootController', function($scope, $rootScope, $window, rootServi
 // matchSenate();
 
 
+
+    var houseMembersCalculation = function() {
+        $scope.houseMissedPercentAvg = 0;
+        var houseMissedNumber = 0;
+
+        $scope.houseVotesWithPartyAvg = 0;
+        var housePartyNumber = 0;
+
+        _.forEach($scope.house.members[0].members, function(member) {
+            if(member.missed_votes_pct){
+                $scope.houseMissedPercentAvg = (parseFloat(member.missed_votes_pct)) + $scope.houseMissedPercentAvg;
+                houseMissedNumber++;
+            }
+
+            if(member.votes_with_party_pct){
+                $scope.houseVotesWithPartyAvg = (parseFloat(member.votes_with_party_pct)) + $scope.houseVotesWithPartyAvg;
+                housePartyNumber++;
+            }
+        })
+        $scope.houseMissedPercentAvg = $scope.houseMissedPercentAvg/houseMissedNumber;
+        $scope.houseVotesWithPartyAvg = $scope.houseVotesWithPartyAvg/housePartyNumber;
+
+        $scope.houseMissedPercentAvg = Math.round($scope.houseMissedPercentAvg * 100) / 100
+        $scope.houseVotesWithPartyAvg = Math.round($scope.houseVotesWithPartyAvg * 100) / 100
+        console.log("+++ 170 root_ctrl.js $scope.houseMissedPercentAvg: ", $scope.houseMissedPercentAvg)
+        console.log("+++ 171 root_ctrl.js $scope.houseVotesWithPartyAvg: ", $scope.houseVotesWithPartyAvg)
+    }
+
+    var senateMembersCalculation = function() {
+        $scope.senateMissedPercentAvg = 0;
+        var senateMissedNumber = 0;
+
+        $scope.senateVotesWithPartyAvg = 0;
+        var senatePartyNumber = 0;
+
+        _.forEach($scope.senate.members[0].members, function(member) {
+            if(member.missed_votes_pct){
+                $scope.senateMissedPercentAvg = (parseFloat(member.missed_votes_pct)) + $scope.senateMissedPercentAvg;
+                senateMissedNumber++;
+            }
+
+            if(member.votes_with_party_pct){
+                $scope.senateVotesWithPartyAvg = (parseFloat(member.votes_with_party_pct)) + $scope.senateVotesWithPartyAvg;
+                senatePartyNumber++;
+            }
+        })
+        $scope.senateMissedPercentAvg = $scope.senateMissedPercentAvg/senateMissedNumber;
+        $scope.senateVotesWithPartyAvg = $scope.senateVotesWithPartyAvg/senatePartyNumber;
+
+        $scope.senateMissedPercentAvg = Math.round($scope.senateMissedPercentAvg * 100) / 100
+        $scope.senateVotesWithPartyAvg = Math.round($scope.senateVotesWithPartyAvg * 100) / 100
+        console.log("+++ 197 root_ctrl.js $scope.senateMissedPercentAvg: ", $scope.senateMissedPercentAvg)
+        console.log("+++ 198 root_ctrl.js $scope.senateVotesWithPartyAvg: ", $scope.senateVotesWithPartyAvg)
+    }
+
+    houseMembersCalculation();
+    senateMembersCalculation();
 });
 
