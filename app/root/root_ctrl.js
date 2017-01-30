fc59cc33d070d6818d05d127f9340b574d91e624
Daniel <daniel@ubermedia.com>
2017-01-29 21:48:11 -0800

    Modularize match committees to members function

--- app/root/root_ctrl.js
+++ app/root/root_ctrl.js
@@ -2,6 +2,9 @@ var app = angular.module('app');
 
 app.controller('rootController', function($scope, $rootScope, $window, rootServices, houseCommittees, senateCommittees, senateServices, houseServices) {
 
+    $scope.senate = senateServices;
+    $scope.house = houseServices;
+
     if ($window.localStorage["countoncongress-userSignedIn"]) {
         $scope.userData = $rootScope.twitterData;
     }
@@ -80,68 +83,6 @@ app.controller('rootController', function($scope, $rootScope, $window, rootServi
     }
 
     getTwitterData();
-// THIS CODE BELOW PULLS CONGRESS' COMMISSIONS AND MEMBERS DATA FROM PROPUBLICA AND MERGES IT PER HOUSE. TO RUN IT: UN COMMENT matchHouse() OR matchSenate() AND LOAD THE PAGE. THE MERGED DATA WILL SHOW UP ON THE CONSOLE.
-
-$scope.senate = senateServices;
-$scope.house = houseServices;
-
-
-$scope.matchedHouse = [];
-$scope.matchedSenate = [];
-
-var matchHouse = function() {
-    _.forEach($scope.house.committees[0].committees, function(committee) {
-
-        var matchedData = {
-            committee: committee,
-            chair: null,
-            ranking_member: null
-
-        };
-
-        _.forEach($scope.house.members[0].members, function(member) {
-            if (member.id === committee.chair_id) {
-                matchedData.chair = member;
-            }
-            if (member.id === committee.ranking_member_id) {
-                matchedData.ranking_member = member;
-            }
-
-        })
-        $scope.matchedHouse.push(matchedData)
-    })
-    console.log("+++ 30 root_ctrl.js $scope.matchedHouse: ", JSON.stringify($scope.matchedHouse, null, "\t"));
-}
-
-
-
-var matchSenate = function() {
-    _.forEach($scope.senate.committees[0].committees, function(committee) {
-
-        var matchedData = {
-            committee: committee,
-            chair: null
-        };
-
-        _.forEach($scope.senate.members[0].members, function(member) {
-            if (member.id === committee.chair_id) {
-                matchedData.chair = member;
-                matchedData.chair.message = "@" + member.twitter_account + " ";
-            }
-            if (member.id === committee.ranking_member_id) {
-                matchedData.ranking_member = member;
-                matchedData.ranking_member.message = "@" + member.twitter_account + " ";
-            }
-        })
-        $scope.matchedSenate.push(matchedData)
-    })
-    console.log("+++ 30 root_ctrl.js $scope.matchedSenate: ", JSON.stringify($scope.matchedSenate, null, "\t"));
-}
-//ONLY RUN THESE WHEN YOU WANT HAVE GATHERED THE NEW CONGRESS DATA FROM PROPUBLICA 
-// matchHouse();
-// matchSenate();
-
-
 
     var houseMembersCalculation = function() {
         $scope.houseMissedPercentAvg = 0;
@@ -151,18 +92,18 @@ var matchSenate = function() {
         var housePartyNumber = 0;
 
         _.forEach($scope.house.members[0].members, function(member) {
-            if(member.missed_votes_pct){
+            if (member.missed_votes_pct) {
                 $scope.houseMissedPercentAvg = (parseFloat(member.missed_votes_pct)) + $scope.houseMissedPercentAvg;
                 houseMissedNumber++;
             }
 
-            if(member.votes_with_party_pct){
+            if (member.votes_with_party_pct) {
                 $scope.houseVotesWithPartyAvg = (parseFloat(member.votes_with_party_pct)) + $scope.houseVotesWithPartyAvg;
                 housePartyNumber++;
             }
         })
-        $scope.houseMissedPercentAvg = $scope.houseMissedPercentAvg/houseMissedNumber;
-        $scope.houseVotesWithPartyAvg = $scope.houseVotesWithPartyAvg/housePartyNumber;
+        $scope.houseMissedPercentAvg = $scope.houseMissedPercentAvg / houseMissedNumber;
+        $scope.houseVotesWithPartyAvg = $scope.houseVotesWithPartyAvg / housePartyNumber;
 
         $scope.houseMissedPercentAvg = Math.round($scope.houseMissedPercentAvg * 100) / 100
         $scope.houseVotesWithPartyAvg = Math.round($scope.houseVotesWithPartyAvg * 100) / 100
@@ -176,18 +117,18 @@ var matchSenate = function() {
         var senatePartyNumber = 0;
 
         _.forEach($scope.senate.members[0].members, function(member) {
-            if(member.missed_votes_pct){
+            if (member.missed_votes_pct) {
                 $scope.senateMissedPercentAvg = (parseFloat(member.missed_votes_pct)) + $scope.senateMissedPercentAvg;
                 senateMissedNumber++;
             }
 
-            if(member.votes_with_party_pct){
+            if (member.votes_with_party_pct) {
                 $scope.senateVotesWithPartyAvg = (parseFloat(member.votes_with_party_pct)) + $scope.senateVotesWithPartyAvg;
                 senatePartyNumber++;
             }
         })
-        $scope.senateMissedPercentAvg = $scope.senateMissedPercentAvg/senateMissedNumber;
-        $scope.senateVotesWithPartyAvg = $scope.senateVotesWithPartyAvg/senatePartyNumber;
+        $scope.senateMissedPercentAvg = $scope.senateMissedPercentAvg / senateMissedNumber;
+        $scope.senateVotesWithPartyAvg = $scope.senateVotesWithPartyAvg / senatePartyNumber;
 
         $scope.senateMissedPercentAvg = Math.round($scope.senateMissedPercentAvg * 100) / 100
         $scope.senateVotesWithPartyAvg = Math.round($scope.senateVotesWithPartyAvg * 100) / 100
@@ -195,5 +136,43 @@ var matchSenate = function() {
 
     houseMembersCalculation();
     senateMembersCalculation();
-});
 
+
+    // THIS CODE BELOW PULLS CONGRESS' COMMISSIONS AND MEMBERS DATA FROM PROPUBLICA AND MERGES IT PER HOUSE. TO RUN IT: UN COMMENT matchHouse() OR matchSenate() AND LOAD THE PAGE. THE MERGED DATA WILL SHOW UP ON THE CONSOLE.
+
+    $scope.matchedChamber = [];
+
+    var matchCommWithMembers = function(chamber) {
+        _.forEach(chamber.committees[0].committees, function(committee) {
+
+            var matchedData = {
+                committee: committee,
+                chair: null,
+                ranking_member: null
+
+            };
+
+            _.forEach(chamber.members[0].members, function(member) {
+                if (member.id === committee.chair_id) {
+                    matchedData.chair = member;
+                    matchedData.chair.message = "@" + member.twitter_account + " ";
+                }
+                if (member.id === committee.ranking_member_id) {
+                    matchedData.ranking_member = member;
+                    matchedData.ranking_member.message = "@" + member.twitter_account + " ";
+                }
+
+            })
+            $scope.matchedChamber.push(matchedData)
+        })
+        console.log("+++ 163 root_ctrl.js chamber: ", chamber.committees[0].chamber)
+        console.log("+++ 30 root_ctrl.js $scope.matchedChamber: ", JSON.stringify($scope.matchedChamber, null, "\t"));
+    }
+
+
+    //ONLY RUN THESE WHEN YOU WANT HAVE GATHERED THE NEW CONGRESS DATA FROM PROPUBLICA 
+    // matchCommWithMembers($scope.house);
+    // matchCommWithMembers($scope.senate);
+
+    //END OF MATCHING CODE
+});
