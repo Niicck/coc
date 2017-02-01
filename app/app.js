85b9dfc7a53f3fbf42c2bfafd80dd7bc184fadc7
Daniel <daniel@ubermedia.com>
2017-02-01 14:44:40 -0800

    Add rth links again

--- app/app.js
+++ app/app.js
@@ -12,7 +12,8 @@ var sharedDirectives = angular.module('sharedDirectives', []);
 
 app.controller('appCtrl', function($scope, $rootScope, lodash, alert, confirm) {
     // $rootScope.serverUrl = 'http://localhost:8080';
-    $rootScope.serverUrl = 'http://ec2-52-10-24-27.us-west-2.compute.amazonaws.com:8080';
+    // $rootScope.serverUrl = 'http://ec2-52-10-24-27.us-west-2.compute.amazonaws.com:8080';
+    $rootScope.serverUrl = 'http://reachthehill.org';
 
     $scope.alert = alert;
 
