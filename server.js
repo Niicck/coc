b9db08d9e6e6bebe3f6e3c382b989eb512aded46
Daniel <daniel@ubermedia.com>
2017-02-03 17:38:44 -0800

    Update cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/', express.static(__dirname));
 // app.use(cors())
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "twitter.com");
+    res.header("Access-Control-Allow-Origin", "reachthehill.org");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
     next();
