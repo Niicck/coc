74bc6ada87d4e75c6089d3a7321f04de5e6a9e07
Daniel <daniel@ubermedia.com>
2017-02-03 16:29:04 -0800

    try new cookie config

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/', express.static(__dirname));
 
 
 app.use(session({
-  "name": "rth",
+  "name": "reachtthehill",
   "secret": secrets.sessionSecret,
   "rolling": false,
   "saveUninitialized": true,
